04910466b0089f4b29229166c25d0821a9ff1762
rothvann2000@gmail.com <rothvann2000@gmail.com>
2018-04-29 03:21:08 -0700

    Added example usage in main

--- OrangeVision/src/Localizer.cpp
+++ OrangeVision/src/Localizer.cpp
@@ -11,22 +11,22 @@
 
 nlohmann::json testFunc(cv::Mat& img) {
 	nlohmann::json json;
-	json["id"] = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now().time_since_epoch()).count();
 	cv::imshow("test", img);
 	cv::waitKey(1);
-	std::cout << "working" << std::endl;
 	return json;
 }
 
 int main() {
 	std::shared_ptr<CameraUtils::ConcurrentMat> stream = std::make_shared<CameraUtils::ConcurrentMat>();
 	ImageReader reader(stream);
-	reader.open(0);
-	ImageProcessor processor(stream);
-	processor.setProcessingFunction(&testFunc);
-	reader.start();
-	processor.start();
-
+	reader.open(0); // Writes VideoCapture(0).read() into ConcurrentMat
+	ImageProcessor processor(stream); // Reads from ConcurrentMat
+	processor.setProcessingFunction(&testFunc); // Set whatever function you want the processor to call.
+	// It needs to take in a cv::Mat& and return a nlohmann::json. nlohmann::json is an object from a 
+	// header only json library we use. 
+	reader.start(); // start reading thread
+	processor.start(); // start processing thread
+	//call reader.stop() or processor.stop() to close thread. These threads are guaranteed to close in 200 ms.
 	//Wait is here because reader and processor are cleaned up automatically when the scope exits (aka when the destructor is called)
 	while (true) {
 
